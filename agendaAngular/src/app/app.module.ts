1fed4a3eb16b522ceff5766b6a9e848026d2f46e
Larissa Novaes <larissanovaes00@hotmail.com>
2020-08-25 17:18:58 -0300

    including styles, icons, fonts and mixins

--- agendaAngular/src/app/app.module.ts
+++ agendaAngular/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { ModalModule } from 'ngx-bootstrap/modal';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ContactsListComponent } from './components/contacts-list/contacts-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    ContactsListComponent
   ],
   imports: [
     BrowserModule,
